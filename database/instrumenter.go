bb0d67d6cfc442d678623562c0b85f5b151aae39
Romesh McCullough <romi@promoboxx.com>
2020-04-25 10:12:29 -0400

    remove newrelic and fix logrus import

--- database/instrumenter.go
+++ database/instrumenter.go
@@ -8,5 +8,15 @@ type DBInstrumentTimer interface {
 
 // StartDBTimer starts a special DB timer and returns something that must be End()-ed
 func StartDBTimer(ctx context.Context, collection, operation, query string) DBInstrumentTimer {
-	return newrelicStartDBTimer(ctx, collection, operation, query)
+	return startNullDBTimer(ctx, collection, operation, query)
+}
+
+type nullDBTimer struct{}
+
+func (nullDBTimer) End() error {
+	return nil
+}
+
+func startNullDBTimer(ctx context.Context, collection, operation, query string) nullDBTimer {
+	return nullDBTimer{}
 }
--- instrumenter/instrumenter.go
+++ instrumenter/instrumenter.go
@@ -3,8 +3,6 @@ package instrumenter
 import (
 	"context"
 	"net/http"
-
-	newrelic "github.com/newrelic/go-agent"
 )
 
 // Client is a weak wrapper around the NR application 'start transaction' functionality, so we don't
@@ -29,26 +27,3 @@ type InstrumentTimer interface {
 type ExternalCallTimer interface {
 	End() // Stop the timer
 }
-
-func NewClientFromNR(app newrelic.Application) Client {
-	return &newrelicClient{app: app}
-}
-
-// StartTimer starts a timer and returns something that must be End()-ed
-func StartTimer(ctx context.Context, name string) InstrumentTimer {
-	return newrelicStartTimer(ctx, name)
-}
-
-// StartExternalCallTimer starts a timer for an outgoing HTTP call
-// and returns something that must be End()-ed
-func StartExternalCallTimer(ctx context.Context, serviceName string, path string) ExternalCallTimer {
-	return newrelicStartExternalTimer(ctx, serviceName, path)
-}
-
-// Add an attribute to the transaction stored in the context, if there is one.
-func AddTxnAttribute(ctx context.Context, key string, value interface{}) error {
-	return newrelicAddAddtribute(ctx, key, value)
-}
-
-// TODO: add metric/event
-// https://github.com/newrelic/go-agent/blob/master/GUIDE.md#custom-events
